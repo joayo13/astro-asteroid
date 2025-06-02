87dfb02bdbaff753e74c0c0f797f301051331dbf
Jordan Ayotte <jordanayotte13@outlook.com>
2025-06-02 12:29:26 -0400

    added backdrop blur to nav, needs tweaking

--- src/scripts/mobileMenu.ts
+++ src/scripts/mobileMenu.ts
@@ -5,12 +5,12 @@ const toggleMobileNav = () => {
     if (!hamburger.checked) {
         mobileNavLinks.classList.add("pointer-events-none");
         mobileNavLinks.classList.add("opacity-0");
-        mobileNavLinks.classList.add("no-doc-scroll");
+        mobileNavLinks.classList.remove("no-doc-scroll");
     }
     else {
         mobileNavLinks.classList.remove("pointer-events-none");
         mobileNavLinks.classList.remove("opacity-0");
-        mobileNavLinks.classList.remove("no-doc-scroll");
+        mobileNavLinks.classList.add("no-doc-scroll");
     }
 };
 
