36f772019a01cb991cfa254f107fe7a12356d32a
Jordan Ayotte <jordanayotte13@outlook.com>
2025-06-17 13:19:25 -0400

    removed navbar popping up and down behavior

--- src/scripts/mobileMenu.ts
+++ src/scripts/mobileMenu.ts
@@ -1,23 +1,26 @@
-const hamburger = document.querySelector(".hamburger-menu input") as HTMLInputElement;
-const mobileNavLinks = document.querySelector("#mobile-nav-links") as HTMLElement;
+const hamburger = document.querySelector(
+  ".hamburger-menu input"
+) as HTMLInputElement;
+const mobileNavLinks = document.querySelector(
+  "#mobile-nav-links"
+) as HTMLElement;
 
 const toggleMobileNav = () => {
-    if (!hamburger.checked) {
-        mobileNavLinks.classList.add("pointer-events-none");
-        mobileNavLinks.classList.add("opacity-0");
-        mobileNavLinks.classList.remove("no-doc-scroll");
-    }
-    else {
-        mobileNavLinks.classList.remove("pointer-events-none");
-        mobileNavLinks.classList.remove("opacity-0");
-        mobileNavLinks.classList.add("no-doc-scroll");
-    }
+  if (!hamburger.checked) {
+    mobileNavLinks.classList.add("pointer-events-none");
+    mobileNavLinks.classList.add("opacity-0");
+    mobileNavLinks.classList.remove("no-doc-scroll");
+  } else {
+    mobileNavLinks.classList.remove("pointer-events-none");
+    mobileNavLinks.classList.remove("opacity-0");
+    mobileNavLinks.classList.add("no-doc-scroll");
+  }
 };
 
 hamburger.addEventListener("click", toggleMobileNav);
 const mobileLinks = document.querySelectorAll(".mobile-link");
-mobileLinks.forEach(link => {
-    link.addEventListener("click", () => {
-        toggleMobileNav();
-    });
+mobileLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    toggleMobileNav();
+  });
 });
